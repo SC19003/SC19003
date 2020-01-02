4b193a69f5d4016819948a2c259860c9a7945bff
SC19003 <3219851710@qq.com>
2020-01-02 19:37:38 +0800

    first commit

---
+++ R/queue1.R
@@ -0,0 +1,51 @@
+#' @title Calculating the Main Quantitative Indicators of Queuing Service System.
+#' @description Queuing problem.
+#' @param lambda The successive arrival intervals of customers to the system are independent and subject to an exponential distribution with a parameter of $\lambda$. (numeric)
+#' @param mu The service time of the service desk is also iid., and obeys the exponential distribution of parameter $\mu$. (numeric)
+#' @param T System service time (numeric)
+#' @return  the length Ls, the average stay time Ws and the probability of customer waiting Pwait.
+#' @examples
+#' \dontrun{
+#' lambda <- 4
+#' mu <- 6
+#' T <- 1000
+#' res <- queue1(4,6,1000)
+#' }
+#' @export
+queue1<-function(lambda, mu, T){
+  k<-0; wt<-0; wn<-0; ws<-0;
+  tp<-0; nA<-0; n<-0; t<-0
+  r<-runif(1); tA<--1/lambda*log(r); tD<-Inf
+  repeat{
+    k<-k+1; wt[k]<-t; wn[k]<-n
+    if (tA < T){
+      ws[k]<-min(tA, tD)-t
+      if (tA < tD){
+        t<-tA; n<-n+1; nA<-nA+1
+        r<-runif(1); tA<-t-1/lambda*log(r)
+        if (n==1){
+          r<-runif(1); tD<-t-1/mu*log(r)
+        }
+      }else{
+        t<-tD; n<-n-1
+        if (n==0){
+          tD<-Inf
+        }else{
+          r<-runif(1); tD<-t-1/mu*log(r)
+        } }
+    }else{
+      ws[k]<-if(tD==Inf) 0 else tD-t
+      if (n>0){
+        t<-tD; n<-n-1
+        if (n>0){
+          r<-runif(1); tD<-t-1/mu*log(r)
+        }
+      }else
+        tp<-1
+    }
+    
+    if (tp==1) break
+  }
+  data.frame(Ls=sum(ws*wn)/t, Ws=sum(ws*wn)/nA,
+             Pwait=sum(ws[wn>=1])/t)
+}
---
+++ R/queue2.R
@@ -0,0 +1,68 @@
+#' @title Calculating the Main Quantitative Indicators of Queuing Service System.
+#' @description Queuing problem.
+#' @param lambda The successive arrival intervals of customers to the system are independent and subject to an exponential distribution with a parameter of $\lambda$. (numeric)
+#' @param mu The service time of the service desk is also iid., and obeys the exponential distribution of parameter $\mu$. (numeric)
+#' @param T System service time (numeric)
+#' @param S The number of the service desk (numeric)
+#' @return  the length Ls, the average stay time Ws and the probability of customer waiting Pwait.
+#' @examples
+#' \dontrun{
+#' lambda <- 4
+#' mu <- 6
+#' T <- 10000
+#' S <- 4
+#' res <- queue2(4,6,10000,4)
+#' }
+#' @export
+queue2<-function(lambda, mu, T, S){
+  k<-0; wt<-0; wn<-0; ws<-0
+  tp<-0; nA<-0; t<-0
+  r<-runif(1); tA<--1/lambda*log(r)
+  tD<-rep(Inf, S); SS<-rep(0, S+1)
+  repeat{
+    t1<-if(SS[1]==0) Inf else min(tD)
+    i1<-if(SS[1]==0) 1 else which.min(tD)
+    k<-k+1; wt[k]<-t; wn[k]<-SS[1]
+    if (tA < T){
+      ws[k]<-min(tA, t1)-t
+      if (tA < t1){
+        t<-tA; nA<-nA+1
+        r<-runif(1); tA<-t-1/lambda*log(r)
+        n<-SS[1]; SS[1]<-n+1
+        for (i in 1:S){
+          if (SS[1+i]==0){
+            SS[1+i]<-1
+            r<-runif(1); tD[i]<-t-1/mu*log(r)
+            break
+          }
+        }
+        
+      }else{
+        t<-t1; n<-SS[1]; SS[1]<-n-1
+        if (n==1){
+          SS[2:(S+1)]<-0; tD[1:S]<-Inf
+        }else if (n<=S){
+          SS[1+i1]<-0; tD[i1]<-Inf
+        }else{
+          r<-runif(1); tD[i1]<-t-1/mu*log(r)
+        } }
+    }else{
+      ws[k]<- if( t1==Inf) 0 else t1-t
+      n<-SS[1]
+      if (n>0){
+        t<-t1; SS[1]<-n-1;
+        if (n==1){
+          SS[2:(S+1)]<-0; tD[1:S]<-Inf
+        }else if (n<=S){
+          SS[1+i1]<-0; tD[i1]<-Inf
+        }else{
+          r<-runif(1); tD[i1]<-t-1/mu*log(r)
+        }
+      }else
+        tp<-1
+    }
+    if (tp==1) break
+  }
+  data.frame(Ls=sum(ws*wn)/t, Ws=sum(ws*wn)/nA,
+             Pwait=sum(ws[wn>=S])/t)
+}
